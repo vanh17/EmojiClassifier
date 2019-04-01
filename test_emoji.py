1f5e0366ab5f471351bdd5139112d8e53983e8dd
Hoang Van <vanh17@hanover.edu>
2019-03-31 17:36:46 -0700

    created inital empty tests

--- test_emoji.py
+++ test_emoji.py
@@ -6,12 +6,16 @@ import emoji
 
 @pytest.fixture(autouse=True)
 def test_read_smsspam():
+    assert 0 == 0
 
 def test_features():
+    assert 0 == 0
 
 def test_labels():
+    assert 0 == 0
 
 def test_prediction(capsys, min_f1=0.89, min_accuracy=0.97):
+    assert 0 == 0
 
 @pytest.mark.xfail
 def test_very_accurate_prediction():
