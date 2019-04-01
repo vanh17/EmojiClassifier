71d2986da511e61d76153b21d75e9ab583344eb9
Van Nguyen Hung Hoang <vanh17@users.noreply.github.com>
2019-04-02 01:01:04 +0700

    delete the scorer We do not need to implement macro from scracht

--- test_emoji.py
+++ test_emoji.py
@@ -4,8 +4,6 @@ from sklearn.metrics import f1_score, accuracy_score
 
 # import our classification class
 import emoji
-# import our scorer_semeval18 code
-import scorer_semeval18
 
 @pytest.fixture(autouse=True)
 def test_read_tweet():
@@ -17,9 +15,48 @@ def test_features():
 def test_labels():
     assert 0 == 0
 
-def test_prediction(capsys, min_f1=0.89, min_accuracy=0.97):
-    assert 0 == 0
+def test_prediction(capsys, min_f1=0.16):
+	truth_file_lines = open("dataset/us_test.labels", encoding='utf8').readlines()
+	gold_keys = np.zeros(len(truth_file_lines), dtype=int)
+    # populating the dictionary one entry for each emoji that appears in our gold_key
+    for i in range(len(truth_file_lines)):
+        emoji_code_gold = int(truth_file_lines[i].replace("\n",""))
+        gold_keys[i] = emoji_code_gold
+    # get texts and labels from the training data
+    train_examples = emoji.read_tweet("dataset/us_train.text", "dataset/us_train.labels")
+    train_labels, train_texts = zip(*train_examples)
+
+    # get texts and labels from the development data
+    devel_examples = classify.read_tweet("dataset/us_dev.text", "dataset/us_dev.labels")
+    devel_labels, devel_texts = zip(*devel_examples)
+
+    # create the feature extractor and label encoder
+    to_features = classify.TextToFeatures(train_texts)
+    to_labels = classify.TextToLabels(train_labels)
+
+    # train the classifier on the training data
+    classifier = classify.Classifier()
+    classifier.train(to_features(train_texts), to_labels(train_labels))
+
+    # make predictions on the development data
+    predicted_indices = classifier.predict(to_features(devel_texts))
+    assert np.array_equal(predicted_indices, predicted_indices.astype(bool))
+
+    # measure performance of predictions
+    devel_indices = to_labels(devel_labels)
+    spam_label = to_labels.index("spam")
+    f1 = f1_score(devel_indices, predicted_indices, average="macro")
+    # accuracy = accuracy_score(devel_indices, predicted_indices)
+
+    # print out performance
+    if capsys is not None:
+        with capsys.disabled():
+            msg = "\n{:.1%} macro-F1 on dataset development data"
+            print(msg.format(f1))
+
+    # make sure that performance is adequate
+    assert f1 > min_f1
 
 @pytest.mark.xfail
 def test_very_accurate_prediction():
-    test_prediction(capsys=None, min_f1=0.94, min_accuracy=0.98)
+    test_prediction(capsys=None, min_f1=0.27)
