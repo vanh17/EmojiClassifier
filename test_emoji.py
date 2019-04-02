be97ba6fe56aec121d008e4eba882212d0ed9542
Van Nguyen Hung Hoang <vanh17@users.noreply.github.com>
2019-04-02 20:52:39 +0700

    remove the gold_key collector.
    Will use the feature extration provided by the Classifier class

--- test_emoji.py
+++ test_emoji.py
@@ -15,19 +15,21 @@ def test_features():
 def test_labels():
     assert 0 == 0
 
-def test_prediction(capsys, min_f1=0.16):
-	truth_file_lines = open("dataset/us_test.labels", encoding='utf8').readlines()
-	gold_keys = np.zeros(len(truth_file_lines), dtype=int)
-    # populating the dictionary one entry for each emoji that appears in our gold_key
-    for i in range(len(truth_file_lines)):
-        emoji_code_gold = int(truth_file_lines[i].replace("\n",""))
-        gold_keys[i] = emoji_code_gold
+def test_prediction(capsys, min_f1=0.5):
+	# truth_file_lines = open("dataset/us_test.labels", encoding='utf8').readlines()
+	# gold_keys = np.zeros(len(truth_file_lines), dtype=int)
+ #    # populating the dictionary one entry for each emoji that appears in our gold_key
+ #    for i in range(len(truth_file_lines)):
+ #        emoji_code_gold = int(truth_file_lines[i].replace("\n",""))
+ #        gold_keys[i] = emoji_code_gold
     # get texts and labels from the training data
     train_examples = emoji.read_tweet("dataset/us_train.text", "dataset/us_train.labels")
     train_labels, train_texts = zip(*train_examples)
 
-    # get texts and labels from the development data
-    devel_examples = classify.read_tweet("dataset/us_dev.text", "dataset/us_dev.labels")
+    # get texts and labels from the development data 
+    # The organizer provide the test set for development process
+    # to evaluate the whole system we need to submit the output_label_text file to their website
+    devel_examples = classify.read_tweet("dataset/us_test.text", "dataset/us_test.labels")
     devel_labels, devel_texts = zip(*devel_examples)
 
     # create the feature extractor and label encoder
@@ -40,11 +42,9 @@ def test_prediction(capsys, min_f1=0.16):
 
     # make predictions on the development data
     predicted_indices = classifier.predict(to_features(devel_texts))
-    assert np.array_equal(predicted_indices, predicted_indices.astype(bool))
 
     # measure performance of predictions
     devel_indices = to_labels(devel_labels)
-    spam_label = to_labels.index("spam")
     f1 = f1_score(devel_indices, predicted_indices, average="macro")
     # accuracy = accuracy_score(devel_indices, predicted_indices)
 
@@ -59,4 +59,4 @@ def test_prediction(capsys, min_f1=0.16):
 
 @pytest.mark.xfail
 def test_very_accurate_prediction():
-    test_prediction(capsys=None, min_f1=0.27)
+    test_prediction(capsys=None, min_f1=0.7)
