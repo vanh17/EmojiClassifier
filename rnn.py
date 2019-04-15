d6f5edd551a5c576f880920edbd09cb7192deb8b
Van Nguyen Hung Hoang <vanh17@users.noreply.github.com>
2019-04-15 20:23:49 +0700

    Update rnn.py

--- rnn.py
+++ rnn.py
@@ -84,5 +84,5 @@ class RNN:
 
     def predict(self, test_texts: Sequence[Text]):
         self.model = load_model("models/best.hd5")
-        test_feat_matrix = pad_sequences(self.tokenizer.texts_to_sequences(test_texts), maxlen=self.maxlen)
+        test_feat_matrix = pad_sequences(self.tokenizer.texts_to_sequences(test_texts))
         return np.argmax(self.model.predict(test_feat_matrix, batch_size=64, verbose=0), axis=1)
