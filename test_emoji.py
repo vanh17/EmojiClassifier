15525d8a094010457059e3b711713b1337938248
Van Nguyen Hung Hoang <vanh17@users.noreply.github.com>
2019-04-24 03:36:22 +0700

    created test for biLSTM

--- test_emoji.py
+++ test_emoji.py
@@ -62,49 +62,88 @@ def to_file(result: np.ndarray, full_path_to_output_file: str):
 #     # predicted_indices_gold = classifier.predict(to_features(test_tweet))
 #     # to_file(predicted_indices_gold)
 
-train_labels, train_texts = rnn.read_tweet("dataset/us_train.text", "dataset/us_train.labels")
-classifier = rnn.RNN()
-classifier.train(train_texts, train_labels)
+# train_labels, train_texts = rnn.read_tweet("dataset/us_train.text", "dataset/us_train.labels")
+# classifier = rnn.RNN()
+# classifier.train(train_texts, train_labels)
 
-def test_prediction_lstm_dev(capsys, min_f1=0.30):
-    # truth_file_lines = open("dataset/us_test.labels", encoding='utf8').readlines()
-    # gold_keys = np.zeros(len(truth_file_lines), dtype=int)
-    # get texts and labels from the training data
-    # train_labels, train_texts = rnn.read_tweet("dataset/us_train.text", "dataset/us_train.labels")
+# def test_prediction_lstm_dev(capsys, min_f1=0.30):
+#     # truth_file_lines = open("dataset/us_test.labels", encoding='utf8').readlines()
+#     # gold_keys = np.zeros(len(truth_file_lines), dtype=int)
+#     # get texts and labels from the training data
+#     # train_labels, train_texts = rnn.read_tweet("dataset/us_train.text", "dataset/us_train.labels")
 
-    # get texts and labels from the development data 
-    # The organizer provide the test set for development process called trial
-    # to evaluate the whole system we need to submit the output_label_text file to their website
-    devel_labels, devel_texts = rnn.read_tweet("dataset/us_trial.text", "dataset/us_trial.labels")
+#     # get texts and labels from the development data 
+#     # The organizer provide the test set for development process called trial
+#     # to evaluate the whole system we need to submit the output_label_text file to their website
+#     devel_labels, devel_texts = rnn.read_tweet("dataset/us_trial.text", "dataset/us_trial.labels")
 
-    # train the classifier on the training data
-    # classifier = rnn.RNN()
-    # classifier.train(train_texts, train_labels)
+#     # train the classifier on the training data
+#     # classifier = rnn.RNN()
+#     # classifier.train(train_texts, train_labels)
 
-    # make predictions on the development data
-    predicted_indices = classifier.predict(devel_texts)
+#     # make predictions on the development data
+#     predicted_indices = classifier.predict(devel_texts)
 
-    # measure performance of predictions
-    devel_indices = classifier.to_labels(devel_labels)
-    f1 = f1_score(devel_indices, predicted_indices, average="macro")
-    accuracy = accuracy_score(devel_indices, predicted_indices)
+#     # measure performance of predictions
+#     devel_indices = classifier.to_labels(devel_labels)
+#     f1 = f1_score(devel_indices, predicted_indices, average="macro")
+#     accuracy = accuracy_score(devel_indices, predicted_indices)
 
-    # print out performance
-    print("LSTM classifier dev results:")
-    msg = "\n{:.1%} macro-F1 on dataset development data"
-    print(msg.format(f1))
-    msg = "\n{:.1%} accuracy on dataset development data"
-    print(msg.format(accuracy))
+#     # print out performance
+#     print("LSTM classifier dev results:")
+#     msg = "\n{:.1%} macro-F1 on dataset development data"
+#     print(msg.format(f1))
+#     msg = "\n{:.1%} accuracy on dataset development data"
+#     print(msg.format(accuracy))
             
 
-    # make sure that performance is adequate
-    assert f1 > min_f1
-    #making the output file so that we can use that for the final testing
-    # test_tweet = emoji.read_test_tweets("dataset/us_test.text")
-    # predicted_indices_gold = classifier.predict(to_features(test_tweet))
-    # to_file(predicted_indices_gold)
+#     # make sure that performance is adequate
+#     assert f1 > min_f1
+#     #making the output file so that we can use that for the final testing
+#     # test_tweet = emoji.read_test_tweets("dataset/us_test.text")
+#     # predicted_indices_gold = classifier.predict(to_features(test_tweet))
+#     # to_file(predicted_indices_gold)
 
-def test_prediction_lstm_test(capsys, min_f1=0.30):
+# def test_prediction_lstm_test(capsys, min_f1=0.30):
+#     # truth_file_lines = open("dataset/us_test.labels", encoding='utf8').readlines()
+#     # gold_keys = np.zeros(len(truth_file_lines), dtype=int)
+#     # get texts and labels from the training data
+
+#     # get texts and labels from the development data 
+#     # The organizer provide the test set for development process called trial
+#     # to evaluate the whole system we need to submit the output_label_text file to their website
+#     devel_labels, devel_texts = rnn.read_tweet("dataset/us_test.text", "dataset/us_test.labels")
+
+#     # train the classifier on the training data
+#     # classifier = rnn.RNN()
+#     # classifier.train(train_texts, train_labels)
+
+#     # make predictions on the development data
+#     predicted_indices = classifier.predict(devel_texts)
+
+#     # measure performance of predictions
+#     devel_indices = classifier.to_labels(devel_labels)
+#     f1 = f1_score(devel_indices, predicted_indices, average="macro")
+#     accuracy = accuracy_score(devel_indices, predicted_indices)
+
+#     # print out performance
+#     print("LSTM classifier test results:")
+#     msg = "\n{:.1%} macro-F1 on dataset development data"
+#     print(msg.format(f1))
+#     msg = "\n{:.1%} accuracy on dataset development data"
+#     print(msg.format(accuracy))
+
+#     # make sure that performance is adequate
+#     assert f1 > min_f1
+#     #making the output file so that we can use that for the final testing
+#     # test_tweet = emoji.read_test_tweets("dataset/us_test.text")
+#     # predicted_indices_gold = classifier.predict(to_features(test_tweet))
+#     # to_file(predicted_indices_gold)
+
+train_labels, train_texts = biLSTM.read_tweet("dataset/us_trial.text", "dataset/us_trial.labels")
+classifier = biLSTM.BILSTM()
+classifier.train(train_texts, train_labels)
+def test_prediction_bi_lstm_test(capsys, min_f1=0.28):
     # truth_file_lines = open("dataset/us_test.labels", encoding='utf8').readlines()
     # gold_keys = np.zeros(len(truth_file_lines), dtype=int)
     # get texts and labels from the training data
